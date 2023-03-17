896d84858440ed9d3c538b7ccf47c06224e8ae65
yohanna <yohannavilchez02@gmail.com>
2023-03-17 15:07:04 +0000

    Proyecto

---
+++ Facturacion/Datos/ClienteDB.cs
@@ -0,0 +1,99 @@
+﻿using Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Text;
+
+namespace Datos
+{
+    public class ClienteDB
+    {
+        string cadena = "server=localhost; user=root; database=factura2; password=123456;";
+
+        public Cliente DevolverClientePorIdentidad(string identidad)
+        {
+            Cliente cliente = null;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT * FROM cliente WHERE Identidad = @Identidad; ");
+                using (MySqlConnection _conexion = new MySqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando = new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        comando.Parameters.Add("@Identidad", MySqlDbType.VarChar, 25).Value = identidad;
+                        MySqlDataReader dr = comando.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            cliente = new Cliente();
+
+                            cliente.Identidad = identidad;
+                            cliente.Nombre = dr["Nombre"].ToString();
+                            cliente.Telefono = dr["Telefono"].ToString();
+                            cliente.Correo = dr["Correo"].ToString();
+                            cliente.Direccion = dr["Direccion"].ToString();
+                            cliente.FechaNacimiento = Convert.ToDateTime(dr["FechaNacimiento"]);
+                            cliente.EstaActivo = Convert.ToBoolean(dr["EstaActivo"]);
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+            return cliente;
+        }
+
+        public DataTable DevolverClientes()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT * FROM cliente ");
+                using (MySqlConnection _conexion = new MySqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando = new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        MySqlDataReader dr = comando.ExecuteReader();
+                        dt.Load(dr);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+            return dt;
+        }
+
+        public DataTable DevolverClientesPorNombre(string nombre)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT * FROM cliente WHERE Nombre LIKE '%" + nombre + "%'");
+
+                using (MySqlConnection _conexion = new MySqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando = new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        MySqlDataReader dr = comando.ExecuteReader();
+                        dt.Load(dr);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+            return dt;
+        }
+
+    }
+}
---
+++ Facturacion/Vista/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void UsuariosToolStripButton_Click(object sender, EventArgs e)
+        {
+            UsuariosForm userForm = new UsuariosForm();
+            userForm.MdiParent = this;
+            userForm.Show();
+        }
+
+        private void ProductosToolStripButton_Click(object sender, EventArgs e)
+        {
+            ProductosForm productosForm = new ProductosForm();
+            productosForm.MdiParent = this;
+            productosForm.Show();
+        }
+
+        private void NuevaFacturaToolStripButton_Click(object sender, EventArgs e)
+        {
+            FacturaForm facturaForm = new FacturaForm();
+            facturaForm.MdiParent = this;
+            facturaForm.Show();
+        }
+    }
+}
